c6f797c6e066db847a6a7bd25fd19dda869f227c
Yoav Ram <yoavram+github@gmail.com>
2012-10-31 12:35:36 +0200

    heroku setup following https://devcenter.heroku.com/articles/python

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 import highlight
 from flask import Flask, request, render_template
+import os
 
-DEBUG = True
 app = Flask(__name__)
+
 
 def code_to_html(code):
 	classified_text = highlight.analyze_python(code)
 	html = highlight.build_html_page(classified_text, title="Highlight your code")
 	return html
+
 
 @app.route("/",  methods=['GET', 'POST'])
 def index():
@@ -18,5 +20,7 @@ def index():
 		return code_to_html(code)
 
 
-if __name__ == "__main__":
-    app.run(debug=False)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
