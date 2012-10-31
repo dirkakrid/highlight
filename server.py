6e939760efb3d05a3daa8b27655966b1442a5332
Yoav Ram <yoavram+github@gmail.com>
2012-10-31 10:41:20 +0200

    first commit

---
+++ server.py
@@ -0,0 +1,22 @@
+import highlight
+from flask import Flask, request, render_template
+
+DEBUG = True
+app = Flask(__name__)
+
+def code_to_html(code):
+	classified_text = highlight.analyze_python(code)
+	html = highlight.build_html_page(classified_text, title="test")
+	return html
+
+@app.route("/",  methods=['GET', 'POST'])
+def index():
+	if request.method == 'GET':
+		return render_template("index.html")
+	else:
+		code = request.form['code']
+		return code_to_html(code)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
