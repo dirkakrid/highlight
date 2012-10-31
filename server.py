7ac6f8462ffdee7ece4fc6dde42ec257dfed2619
Yoav Ram <yoavram+github@gmail.com>
2012-10-31 21:35:19 +0200

    added the powerpoint fix and changed the colophon

--- server.py
+++ server.py
@@ -11,16 +11,29 @@ def code_to_html(code):
 	return html
 
 
+def powerpoint_fix(html, start_pre = '<pre class="python">', stop_pre = '</pre>'):
+	start = html.find(start_pre) + len(start_pre)
+	stop = html.find(stop_pre, start)
+	code = html[start:stop]
+	code = code.replace("\n","<br>")
+	code = code.replace("\t","&nbsp;&nbsp;&nbsp;&nbsp;")
+	code = code.replace("    ","&nbsp;&nbsp;&nbsp;&nbsp;")
+	return html[:start] + code + html[stop:]
+        
+
 @app.route("/",  methods=['GET', 'POST'])
 def index():
 	if request.method == 'GET':
 		return render_template("index.html")
 	else:
 		code = request.form['code']
-		return code_to_html(code)
+		html = code_to_html(code)
+		if 'pptx' in request.form:
+			html = powerpoint_fix(html)
+		return html
 
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
